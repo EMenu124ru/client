1429daef32d2ff2c0cd0b426aa4ebd76ce4854b6
danil <d.kokh@solutionfactory.ru>
2022-10-24 01:26:23 +0700

    TEAM-COURSE-PROJECT-2022-2023-T-14 Check husky

--- src/features/auth/components/SignUpBackgroundLayout/SignUpBackgroundLayout.tsx
+++ src/features/auth/components/SignUpBackgroundLayout/SignUpBackgroundLayout.tsx
@@ -2,6 +2,7 @@ import { FC, memo, PropsWithChildren } from 'react';
 import './SignUpBackgroundLayout.scss';
 import AuthLogo from '@assets/images/auth-logo.png'
 
+
 export const SignUpBackgroundLayoutComponent: FC<PropsWithChildren> = ({ children }) => (
   <div className="auth__black_shape_background_signup">
     <img src={AuthLogo} alt="No source :(" className="auth__logo" />
