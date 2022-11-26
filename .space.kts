5824effc011b5f6f66d33b8a7eab242782ea09df
Danil Kokh <danil0420022@gmail.com>
2022-11-26 14:34:51 +0000

    Add deploy

--- .space.kts
+++ .space.kts
@@ -14,4 +14,17 @@ job("Run npm test and publish") {
             """
         }
     }
+    
+    container("openjdk:11") {
+    kotlinScript { api ->
+        api.space().projects.automation.deployments.start(
+            project = api.projectIdentifier(),
+            targetIdentifier = TargetIdentifier.Key("ssh-root-62-113-96-162"),
+            version = "1.0.0",
+            // automatically update deployment status based on a status of a job
+            syncWithAutomationJob = true
+        )
+    }
 }
+}
+
