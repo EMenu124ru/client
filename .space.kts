a68d7629203250e103a924021ebd86eca2c30ea3
Danil Kokh <danil0420022@gmail.com>
2022-11-26 12:36:56 +0000

    Test

--- .space.kts
+++ .space.kts
@@ -1,6 +1,6 @@
 job("Run npm test and publish") {
     container(displayName = "Run publish script", image = "node:14-alpine") {
-        env["REGISTRY"] = "https://npm.pkg.jetbrains.space/mycompany/p/projectkey/mynpm"
+        env["REGISTRY"] = "https://npm.pkg.jetbrains.space/ikit-ki20-161-b/p/team-course-project-2022-2023/client/"
         shellScript {
             interpreter = "/bin/sh"
             content = """
