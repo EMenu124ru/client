dd41506bb395e72090a4cf6b363ba9b619f611d4
Danil Kokh <danil0420022@gmail.com>
2022-11-27 14:06:33 +0000

    CI/CD

--- .space.kts
+++ .space.kts
@@ -1,4 +1,17 @@
 job("Run npm test and publish") {
+    startOn {
+        gitPush {
+            // run on changes in all 'release-...'
+            // branches excluding 'release-main'
+            // exclude rules have priority
+            branchFilter {
+                +Regex("main)
+            }
+            pathFilter {
+                +"src/**"
+            }
+        }
+    }
     container(displayName = "Run publish script", image = "node:14-alpine") {
         env["REGISTRY"] = "https://npm.pkg.jetbrains.space/ikit-ki20-161-b/p/team-course-project-2022-2023/client/"
         shellScript {
@@ -20,7 +33,7 @@ job("Run npm test and publish") {
         api.space().projects.automation.deployments.start(
             project = api.projectIdentifier(),
             targetIdentifier = TargetIdentifier.Key("ssh-root-62-113-96-162"),
-            version = "1.0.\$JB_SPACE_EXECUTION_NUMBER",
+            version = "1.0.$JB_SPACE_EXECUTION_NUMBER",
             // automatically update deployment status based on a status of a job
             syncWithAutomationJob = true
         )
