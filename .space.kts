0306d6fa16bbc1d9181792a365dffdd9432bccdc
danil <d.kokh@solutionfactory.ru>
2022-11-30 23:11:23 +0700

    Test

--- .space.kts
+++ .space.kts
@@ -20,18 +20,18 @@ job("Run npm test and publish") {
   }
 
 
-//  host("Build artifacts and a Docker image") {
-//
-//    env["HUB_USER"] = Params("dockerhub_user")
-//    env["HUB_TOKEN"] = Secrets("dockerhub_token")
-//    env["SPACE_REPO"] = "ikit-ki20-161-b.registry.jetbrains.space/p/team-course-project-2022-2023/frontend-client"
+  host("Build artifacts and a Docker image") {
 
-//    shellScript {
-//      // login to Docker Hub
-//      content = """
-//                    docker login ${'$'}SPACE_REPO -u ${'$'}HUB_USER --password "${'$'}HUB_TOKEN"
-//                """
-//    }
+    env["HUB_USER"] = Params("dockerhub_user")
+    env["HUB_TOKEN"] = Secrets("dockerhub_token")
+    env["SPACE_REPO"] = "ikit-ki20-161-b.registry.jetbrains.space/p/team-course-project-2022-2023/frontend-client"
+
+    shellScript {
+      // login to Docker Hub
+      content = """
+                    docker login ${'$'}SPACE_REPO -u ${'$'}HUB_USER --password "${'$'}HUB_TOKEN"
+                """
+    }
 
     dockerBuildPush {
       // Docker context, by default, project root
