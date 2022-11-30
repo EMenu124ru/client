e91b8e499ce59ee83f4ed77bf4120ae914810026
Danil Kokh <danil0420022@gmail.com>
2022-11-30 15:38:21 +0000

    Test

--- .space.kts
+++ .space.kts
@@ -1,10 +1,11 @@
 job("Run npm test and publish") {
     
-    env["HUB_USER"] = Params("dockerhub_user")
-    env["HUB_TOKEN"] = Secrets("dockerhub_token")
-    env["SPACE_REPO"] = "ikit-ki20-161-b.registry.jetbrains.space/p/team-course-project-2022-2023/frontend-client"
-    
     host("Build artifacts and a Docker image") {
+        
+        env["HUB_USER"] = Params("dockerhub_user")
+        env["HUB_TOKEN"] = Secrets("dockerhub_token")
+        env["SPACE_REPO"] = "ikit-ki20-161-b.registry.jetbrains.space/p/team-course-project-2022-2023/frontend-client"
+
         
         failOn {
             testFailed {  enabled = false  }
