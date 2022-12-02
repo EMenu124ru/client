96a0fdd4ca60bed428cba3af358d84a29515b737
Danil Kokh <danil0420022@gmail.com>
2022-12-02 17:08:48 +0000

    Test

--- .space.kts
+++ .space.kts
@@ -26,24 +26,26 @@ job("Run npm test and publish") {
     env["SSH_PRIVATE_KEY"] = Secrets("ssh-private")
     env["SPACE_REPO"] = "ikit-ki20-161-b.registry.jetbrains.space/p/team-course-project-2022-2023/frontend-client"
 
-    shellScript {
+    //shellScript {
       // login to Docker Hub
-      content = """
-                    docker login ${'$'}SPACE_REPO -u ${'$'}HUB_USER --password "${'$'}HUB_TOKEN"
-                """
-    }
+    //  content = """
+    //                docker login ${'$'}SPACE_REPO -u ${'$'}HUB_USER --password "${'$'}HUB_TOKEN"
+    //              """
+    //}
 
-    dockerBuildPush {
+    //dockerBuildPush {
       // Docker context, by default, project root
-      file = "Dockerfile"
-      val spaceRepo = "${"$"}SPACE_REPO/main"
-      tags {
-        +"$spaceRepo:1.0.${"$"}JB_SPACE_EXECUTION_NUMBER"
-        +"$spaceRepo:latest"
-      }
-    }
+    //  file = "Dockerfile"
+    //  val spaceRepo = "${"$"}SPACE_REPO/main"
+    //  tags {
+    //    +"$spaceRepo:1.0.${"$"}JB_SPACE_EXECUTION_NUMBER"
+    //    +"$spaceRepo:latest"
+    //  }
+    //}
   }
    container(displayName = "Run myscript", image = "rastasheep/ubuntu-sshd") {
+       env["SSH_PRIVATE_KEY"] = Secrets("ssh-private")
+       env["SPACE_REPO"] = "ikit-ki20-161-b.registry.jetbrains.space/p/team-course-project-2022-2023/frontend-client"
         shellScript {
           content = """
           				command -v ssh-agent >/dev/null || ( apk add --update openssh )
